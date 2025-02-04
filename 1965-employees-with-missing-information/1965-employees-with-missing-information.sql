de5d9dda3613688aa87357021a2006f0c4764aa8
jrx100th <jenith.ch15@gmail.com>
2025-02-04 21:58:13 +0530

    Time: 299 ms (77.61%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,20 +1,4 @@
--- Write your PostgreSQL query statement below
-SELECT * FROM
-(SELECT employee_id FROM Employees 
-WHERE employee_id NOT IN
-(SELECT t1emp FROM
-(SELECT e1.employee_id AS t1emp , name AS t1name , s1.employee_id AS t2emp, salary AS t2sal FROM Employees e1
-JOIN Salaries s1
-ON e1.employee_id = s1.employee_id
-AND s1.employee_id = e1.employee_id
-))
-UNION 
-SELECT employee_id FROM Salaries 
-WHERE employee_id NOT IN
-(SELECT t1emp FROM
-(SELECT e1.employee_id AS t1emp , name AS t1name , s1.employee_id AS t2emp, salary AS t2sal FROM Employees e1
-JOIN Salaries s1
-ON e1.employee_id = s1.employee_id
-AND s1.employee_id = e1.employee_id
-)))
-ORDER BY 1 ASC
+select employee_id from employees where employee_id not in (select employee_id from salaries )
+union
+select employee_id from salaries where employee_id not in (select employee_id from employees)
+order by employee_id;
