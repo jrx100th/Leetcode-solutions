851cf71fd3fad3fa4b6e7355ae189d5c018f3a2b
jrx100th <jenith.ch15@gmail.com>
2025-02-09 15:36:29 +0530

    Time: 664 ms (73.98%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,10 @@
 -- Write your PostgreSQL query statement below
 SELECT 
-user_id, UPPER(LEFT(name,1)) || LOWER(RIGHT(name,LENGTH(name)-1)) AS name
+user_id,
+UPPER(LEFT(name,1)) || LOWER(RIGHT(name,LENGTH(name)-1)) AS name
 FROM 
 Users
 ORDER BY user_id
+
+--user_id, UPPER(LEFT(name,1)) || LOWER(RIGHT(name,LENGTH(name)-1)) AS name
+--initcap(name) AS name -- failed one testcase
