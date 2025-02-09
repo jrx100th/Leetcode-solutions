abeadf0f003cdd3bcc2fe125f343980d96129855
jrx100th <jenith.ch15@gmail.com>
2025-02-09 18:01:02 +0530

    Time: 727 ms (35.28%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+SELECT name FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT sales_id FROM Orders
+    WHERE com_id NOT IN (
+        SELECT com_id FROM Company
+        WHERE name <> 'RED'
+    )
+)
