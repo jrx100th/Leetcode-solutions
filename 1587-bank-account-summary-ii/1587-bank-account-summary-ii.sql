1efb6a21d73de945cb759e69ffaab534d3b91d49
jrx100th <jenith.ch15@gmail.com>
2025-02-11 17:18:14 +0530

    Time: 393 ms (59.31%), Space: 0B (100%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+name AS "NAME",
+SUM(amount) AS "BALANCE"
+FROM
+Users
+INNER JOIN
+Transactions
+ON 
+Users.account = Transactions.account
+GROUP BY name
+HAVING SUM(amount) > 10000
