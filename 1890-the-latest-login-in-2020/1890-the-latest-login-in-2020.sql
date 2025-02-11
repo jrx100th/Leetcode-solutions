e50ee18e30c64a1db5e28fd71c622dd10dc449c5
jrx100th <jenith.ch15@gmail.com>
2025-02-11 19:35:41 +0530

    Time: 449 ms (92.54%), Space: 0B (100%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
-SELECT Logins.user_id, MAX(time_stamp) AS last_stamp
+SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE 
 TO_CHAR(time_stamp, 'YYYY') = '2020'
-GROUP BY Logins.user_id
+GROUP BY user_id
