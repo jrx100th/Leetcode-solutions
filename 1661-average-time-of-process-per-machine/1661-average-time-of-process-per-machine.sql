b6dc91e5023046655cfe2d54f77bb1cb49e19548
jrx100th <jenith.ch15@gmail.com>
2025-02-15 10:25:47 +0530

    Time: 259 ms (77.23%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,21 @@
+SELECT 
+machine_id,
+ROUND(CAST(
+(SUM(
+    CASE
+        WHEN activity_type = 'end' THEN timestamp
+        ELSE 0
+        END
+)
+-
+SUM(
+    CASE
+        WHEN activity_type = 'start' THEN timestamp
+        ELSE 0
+        END)
+)/COUNT(DISTINCT(process_id))
+AS NUMERIC)
+,3) AS processing_time
+FROM 
+Activity
+GROUP BY machine_id
