f6c34e817604e479d6132dcf09b1bd2967b2ddde
jrx100th <jenith.ch15@gmail.com>
2025-02-15 11:02:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,17 @@
+WITH min_dates AS (
+    SELECT 
+        customer_id, 
+        MIN(order_date) AS min_order_date, 
+        MIN(customer_pref_delivery_date) AS min_pref_date
+    FROM Delivery
+    GROUP BY customer_id
+)
+
+SELECT 
+    ((SUM(CASE 
+            WHEN min_order_date = min_pref_date THEN 1.0
+            ELSE 0
+        END)*1.0/
+        COUNT(DISTINCT(customer_id))*1.0)*100) AS immediate_percentage
+FROM min_dates
+;
