dad5ea3b40f13ad2a019194c85ab4be0fb4fc9b9
jrx100th <jenith.ch15@gmail.com>
2025-02-27 10:00:43 +0530

    Time: 494 ms (78.44%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -4,5 +4,5 @@ ROUND(AVG(e1.age),0) AS average_age
 FROM Employees e1
 INNER JOIN Employees e2
 ON e1.reports_to = e2.employee_id
-GROUP BY 1,2
+GROUP BY e2.employee_id,e2.name
 ORDER BY employee_id;
