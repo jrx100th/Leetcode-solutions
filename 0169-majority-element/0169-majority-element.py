4504d883a254a6a11c08a95d265e8e9221b7aaf0
jrx100th <jenith.ch15@gmail.com>
2025-06-07 09:53:27 +0530

    Time: 12 ms (41.01%), Space: 13.8 MB (16.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,11 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        from collections import Counter
+        """from collections import Counter
         count = Counter(nums)
         n = len(nums)
         
         for num, count in count.items():
             if count > (n/2):
-                return num
+                return num"""
+
+        candidate = None
+        nums.sort()
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
         
