f9a73c1b2e6225deaab4db47509130b5cde045a4
jrx100th <jenith.ch15@gmail.com>
2025-06-27 13:38:46 +0530

    Time: 15 ms (42.07%), Space: 18.1 MB (32.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return collections.Counter(s) == collections.Counter(t)
+        mapper = {}
+        for letter in s:
+            if letter not in mapper:
+                mapper[letter] = 0
+            mapper[letter] += 1
+
+        for letter in t:
+            if letter not in mapper:
+                return False
+            if letter in mapper:
+                mapper[letter] -= 1
+
+        mapper =  list(mapper.values())
+
+        for num in mapper:
+            if num != 0:
+                return False
+        return True
