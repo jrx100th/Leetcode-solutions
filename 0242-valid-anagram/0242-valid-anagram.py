2d73bf3d8cd68683a355d3f53ada0532b4f8ca46
jrx100th <jenith.ch15@gmail.com>
2025-06-27 13:35:33 +0530

    Time: 7 ms (89.71%), Space: 17.8 MB (85.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        from collections import Counter
-        return Counter(s) == Counter(t)
+        return collections.Counter(s) == collections.Counter(t)
