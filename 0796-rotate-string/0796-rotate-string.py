043315d1976ab5a334cc7735fbee99e0c04940a2
jrx100th <jenith.ch15@gmail.com>
2025-06-28 19:44:53 +0530

    Time: 0 ms (100%), Space: 17.7 MB (81.51%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,16 +1,9 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        from collections import deque
 
-        if len(s) != len(goal) or len(set(s)) != len(set(goal)):
+        
+        if len(set(s)) != len(set(goal)) or len(s) != len(goal):
             return False
 
-        s = deque(s)
-        x = s
-        for i in range(len(s)):
-            x.rotate(-1)
-            if "".join(x) == goal:
-                return True
-        return False
-            
-        
+        s = s+s
+        return goal in s
